983be6cd0dbab92deb37056279461327ebf3f25e
Manuelbcd <manuelbcd@gmail.com>
2018-10-11 18:08:24 +0200

    code refactorization

--- cmd/mailguard/incomingmail.go
+++ cmd/mailguard/incomingmail.go
@@ -1,11 +1,20 @@
 package main
 
+/**
+More information about emerson IMAP lib
+https://github.com/emersion/go-imap/wiki
+ */
+
 import (
 	"log"
 	"os"
 	"github.com/emersion/go-imap/client"
 	"github.com/emersion/go-imap"
+	"github.com/emirpasic/gods/utils"
 )
+
+const CONST_MAXUNREADMESSAGESPERCYCLE		= 10
+const CONST_INBOXFOLDERNAME					= "INBOX"
 
 /**
 	Open a new IMAP connection and return the connection pointer
@@ -22,22 +31,54 @@ func openIMAPConnection(config Config) (c *client.Client) {
 	return connection
 }
 
+
 /**
 	Check incoming messages
+	TODO: Get message bodies (only envelope right now): https://github.com/emersion/go-imap/wiki/Fetching-messages
  */
 func incomingMail(config Config){
 
+	// Open connection
 	c := openIMAPConnection(cfg)
 
-	// Don't forget to logout
+	// Don't forget to logout at the end of this method
 	defer c.Logout()
 
-	// Login
+	// IMAP authentication
 	if err := c.Login(config.Auth.Username, config.Auth.Password); err != nil {
 		log.Fatal(err)
 	}
 	log.Println("Logged in")
 
+	// Point to Inbox folder
+	mbox := getIMAPFolder(c, CONST_INBOXFOLDERNAME)
+
+	// Get last unread e-mails
+	messages, seqset := getLastUnreadMessages(c, mbox, CONST_MAXUNREADMESSAGESPERCYCLE)
+
+	// Feed the message list
+	done := make(chan error, 1)
+	go func() {
+		done <- c.Fetch(seqset, []imap.FetchItem{imap.FetchEnvelope}, messages)
+	}()
+
+	// Walk around message list
+	log.Println("Last " + utils.ToString(CONST_MAXUNREADMESSAGESPERCYCLE) +  " messages:")
+	for msg := range messages {
+		log.Println("* " + msg.Envelope.Subject)
+		log.Println(msg.Envelope.Date)
+		log.Println()
+	}
+
+	if err := <-done; err != nil {
+		log.Fatal(err)
+	}
+}
+
+/**
+	Return a pointer to a determined mailbox folder
+ */
+func getIMAPFolder(c * client.Client, foldername string) imap.MailboxStatus {
 	// List mailboxes
 	mailboxes := make(chan *imap.MailboxInfo, 10)
 	done := make(chan error, 1)
@@ -54,35 +95,34 @@ func incomingMail(config Config){
 		log.Fatal(err)
 	}
 
-	// Select INBOX
-	mbox, err := c.Select("INBOX", false)
+	// Select folder
+	mbox, err := c.Select(foldername, false)
 	if err != nil {
 		log.Fatal(err)
+		os.Exit(1)
 	}
-	log.Println("Flags for INBOX:", mbox.Flags)
+	log.Println("Flags for " + foldername + ":", mbox.Flags)
 
+	return *mbox
+}
+
+/**
+	Extract a chan struct with last N unread messages.
+ 	@limit means the max number of unread messages each time this method is invoked.
+ */
+func getLastUnreadMessages(c * client.Client, mbox imap.MailboxStatus, limit uint32) (chan*(imap.Message), *imap.SeqSet)  {
 	// Get the last 4 messages
 	from := uint32(1)
 	to := mbox.Messages
-	if mbox.Messages > 3 {
+	if mbox.Messages > limit {
 		// We're using unsigned integers here, only substract if the result is > 0
-		from = mbox.Messages - 3
+		from = mbox.Messages - limit
 	}
 	seqset := new(imap.SeqSet)
 	seqset.AddRange(from, to)
 
 	messages := make(chan *imap.Message, 10)
-	done = make(chan error, 1)
-	go func() {
-		done <- c.Fetch(seqset, []imap.FetchItem{imap.FetchEnvelope}, messages)
-	}()
 
-	log.Println("Last 4 messages:")
-	for msg := range messages {
-		log.Println("* " + msg.Envelope.Subject)
-	}
-
-	if err := <-done; err != nil {
-		log.Fatal(err)
-	}
+	return messages, seqset
 }
+
