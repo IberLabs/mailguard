d81756ced114758587f3019190ace7e8fbceb97f
Manuelbcd <manuelbcd@gmail.com>
2018-10-28 20:36:55 +0100

    Rules evaluation and start with functions

--- internal/rulehandler.go
+++ internal/rulehandler.go
@@ -24,28 +24,34 @@ func EvalRulesAndTriggerActions(config Config, rules []string, messages chan * i
 		fmt.Println(msg.Envelope.Date)
 
 		evalRuleAndTriggerAction(msg, config, rules)
-		
 		fmt.Println()
 	}
 }
 
 /**
-	Evaluate a message and generate an action
+	Evaluate a rule against messages an trigger an action.
+	Both rules and actions are loaded from yml file.
  */
 func evalRuleAndTriggerAction(msg *imap.Message, config Config, rules []string) {
 
+	var rulesMatched = false
+
 	// Walk around rules
 	for _, rule := range rules {
-
 
 		if !strings.Contains( rule, CONST_TAG_EVAL){
 			continue
 		}
 
+		defer func() {
+			if r := recover(); r != nil {
+				fmt.Println("Recovered in after rule fail", r)
+			}
+		}()
+
 		i := strings.Index(rule, CONST_TAG_EVAL)
 		// Trim any empty char or tabulation
 		rawRule := strings.Trim(rule[i+len(CONST_TAG_EVAL):], "	")
-		rawRule = strings.Trim(rawRule, "	")
 		rawRule = strings.Trim(rawRule, " ")
 
 		fmt.Println("Rule detected")
@@ -53,20 +59,34 @@ func evalRuleAndTriggerAction(msg *imap.Message, config Config, rules []string) 
 
 		expression, err := govaluate.NewEvaluableExpression(rawRule);
 		parameters := make(map[string]interface{}, 8)
-		parameters["subject"] = "hello"
+		parameters["subject"] = msg.Envelope.Subject;
+		parameters["sender"] = msg.Envelope.Sender;
+
+		/** TODO: Add functions evaluation */
+
 		result, err := expression.Evaluate(parameters);
-		// result is now set to "false", the bool value
-		_ = result	// temp
-		_ = err		// temp
+
+		if err == nil && result == true {
+			rulesMatched = true
+
+			//Action temporary hardcoded
+			fmt.Println("Rule 1 activated by message: " + msg.Envelope.Subject)
+			fmt.Println("Sending message (Hardcoded for now)")
+
+			// Send emails
+			sendMail(	config, config.Account.Sender, (msg.Envelope.Sender[0].MailboxName + "@" + msg.Envelope.Sender[0].HostName),
+				"Que tal vamos", "Que tal vamos. \r\nque tal estamos")
+			break
+		}
+
 
 	}
-	return //temp
+
 
 
 	// TEMPORARY HARD-CODED---------------------------------------
-
-
 	// Rule 1 : Send e-mail to sender
+	/**
 	if(msg.Envelope.Subject == "hola"){
 		fmt.Println("Rule 1 activated by message: " + msg.Envelope.Subject)
 
@@ -75,6 +95,11 @@ func evalRuleAndTriggerAction(msg *imap.Message, config Config, rules []string) 
 			"Que tal vamos", "Que tal vamos. \r\nque tal estamos")
 
 	}else{
+		fmt.Println("No rules matched")
+	}
+	*/
+
+	if !rulesMatched {
 		fmt.Println("No rules matched")
 	}
 
